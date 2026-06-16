6416a68e18572e496edb7eed432e139d5f6d890e
Chirag Gupta <chiraggupta4yt@gmail.com>
2026-06-17 02:04:54 +0530

    First Array Question

--- p1.py
+++ p1.py
@@ -1,1 +1,14 @@
-print("Hello")
+"""Given an array of integers, find and return the largest element.
+
+Example
+Input: [3, 1, 7, 2, 9, 4] Output: 9
+"""
+
+
+n=list(map(int,input("Enter the numbers:").split()))
+
+max_val=n[0]
+for i in range(1,len(n)):
+    if n[i]>max_val:
+        max_val=n[i]
+print(max_val)
