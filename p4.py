2da5d802f6a24717f397399417b6401399eb0b9e
Chirag Gupta <chiraggupta4yt@gmail.com>
2026-06-22 02:26:14 +0530

    Final Commit

--- p4.py
+++ p4.py
@@ -4,7 +4,15 @@ Example
 
 Input: [10, 20, 4, 45, 99] Output: 45"""
 
-n=list(map(int,input("Enter the number:").split()))
+n = list(map(int, input("Enter the numbers: ").split()))
 
-n.sort()
-print(n[-2])
+first = second = float('-inf')   # negative infinity as starting point
+
+for num in n:
+    if num > first:
+        second = first           # old max becomes second
+        first = num              # new max
+    elif num > second and num != first:  # bigger than second but not a duplicate
+        second = num
+
+print(second)
