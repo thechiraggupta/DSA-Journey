0ea56ff6433509318cd48d6f9b6ea1a61421e02b
Chirag Gupta <chiraggupta4yt@gmail.com>
2026-06-22 01:32:09 +0530

    Sorting Check

---
+++ p3.py
@@ -0,0 +1,13 @@
+""" Given an array, return True if it is sorted in non-decreasing order, else False.
+
+Example
+
+Input: [1, 2, 2, 5, 9] Output: True Input: [3, 1, 4] Output: False"""
+
+n=list(map(int,input("Enter the numbers:").split()))
+
+is_sorted=True
+for i in range (len(n)):
+    if n[i]>n[i+1]:
+        is_sorted=False
+        break
