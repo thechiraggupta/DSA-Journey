8f8ce218d14595d0378420e2427166bf7fc54c37
Chirag Gupta <chiraggupta4yt@gmail.com>
2026-07-09 22:19:48 +0530

    Leetcode

---
+++ p11.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        result = []
+
+        for i in range(len(words)):
+            if x in words[i]:
+                result.append(i)
+
+        return result
