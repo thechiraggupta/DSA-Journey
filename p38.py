b7fef9f75b49ac493c0730def716edd3d8cf3193
Chirag Gupta <chiraggupta4yt@gmail.com>
2026-08-05 23:03:30 +0530

    Leetcode

---
+++ p38.py
@@ -0,0 +1,36 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        i = 0
+        n = len(s)
+
+        # Step 1: Skip leading whitespaces
+        while i < n and s[i] == ' ':
+            i += 1
+
+        # Step 2: Check sign
+        sign = 1
+        if i < n and (s[i] == '+' or s[i] == '-'):
+            if s[i] == '-':
+                sign = -1
+            i += 1
+
+        # Step 3: Skip leading zeros
+        while i < n and s[i] == '0':
+            i += 1
+
+        # Step 4: Convert digits
+        num = 0
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+
+        while i < n and s[i].isdigit():
+            digit = int(s[i])
+
+            # Check for overflow
+            if num > (INT_MAX - digit) // 10:
+                return INT_MAX if sign == 1 else INT_MIN
+
+            num = num * 10 + digit
+            i += 1
+
+        return sign * num
