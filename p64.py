5e63ecd541f2635381f10dfa9c0d042580c00368
Chirag Gupta <chiraggupta4yt@gmail.com>
2026-09-04 17:16:26 +0530

    Leetcode

---
+++ p64.py
@@ -0,0 +1,18 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def deleteDuplicates(self, head):
+        current = head
+
+        while current and current.next:
+            if current.val == current.next.val:
+                # Skip the duplicate node
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return head
